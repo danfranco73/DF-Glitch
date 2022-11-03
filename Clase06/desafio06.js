45c60dbd61bb672cd74669ab6d3255e4b36aa000
danfranco73 <danielfrancotucu@gmail.com>
2022-11-03 18:41:24 -0300

    Update desafio06.js

--- Clase06/desafio06.js
+++ Clase06/desafio06.js
@@ -8,7 +8,7 @@
 
 const express = require("express");
 const app = express();
-const PORT = 8090 || process.env.PORT;  
+const PORT = process.env.PORT || 8080; // default port to listen  
 const fs = require("fs").promises;
 const path = require("path");
 
